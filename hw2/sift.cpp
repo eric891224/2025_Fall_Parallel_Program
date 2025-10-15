02f37bcef3f9a9ab131847453a57acf6526ea40d
eric891224 <s96006730@gmail.com>
2025-10-15 16:28:59 +0800

    remove dist

--- hw2/sift.cpp
+++ hw2/sift.cpp
@@ -10,7 +10,37 @@
 
 #include "sift.hpp"
 #include "image.hpp"
-#include "dist.hpp"
+// #include "dist.hpp"
+
+void allgather_gradient_pyramid(ScaleSpacePyramid &local_grad_pyramid)
+{
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    // ScaleSpacePyramid complete_pyramid = local_grad_pyramid;
+
+    for (int i = 0; i < local_grad_pyramid.num_octaves; i++)
+    {
+        for (int j = 0; j < local_grad_pyramid.imgs_per_octave; j++)
+        {
+            const Image &img = local_grad_pyramid.octaves[i][j];
+
+            // Determine which process computed this image
+            int owner = j % size;
+
+            /*
+            if rank == owner:
+                This process computed this image, broadcast it to others
+            else:
+                This process didn't compute this image, receive it
+            */
+            MPI_Bcast(img.data, img.size, MPI_FLOAT, owner, MPI_COMM_WORLD);
+        }
+    }
+
+    return;
+}
 
 ScaleSpacePyramid generate_gaussian_pyramid(const Image &img, float sigma_min,
                                             int num_octaves, int scales_per_octave)
