ed5e2a7da21cbf7996bf48805f53800cf45918d7
eric891224 <s96006730@gmail.com>
2025-10-17 17:13:02 +0800

    analysis

--- hw2/sbatch_analysis.sh
+++ hw2/sbatch_analysis.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=hw2_analysis
 #SBATCH --account=ACD114118
-#SBATCH --nodes=8                    # Request max nodes you need
-#SBATCH --ntasks=24                  # Request max tasks you need
-#SBATCH --cpus-per-task=24           # Request max CPUs per task
+#SBATCH --partition=ctest
+#SBATCH --nodes=6                    # Request max nodes you need
+#SBATCH --ntasks=12                  # Request max tasks you need
+#SBATCH --cpus-per-task=32           # Request max CPUs per task
 #SBATCH --time=01:00:00
 #SBATCH --output=output.log
 #SBATCH --error=error.log
