d1dccddbbc8b7815a3f02c1c5bc7fc72e3394e4a
eric891224 <s96006730@gmail.com>
2025-11-12 14:09:10 +0800

    finish hw3

--- hw3/run_gpu.sh
+++ hw3/run_gpu.sh
@@ -8,8 +8,14 @@ read x1 y1 z1 <<< "$pos"
 read x2 y2 z2 <<< "$tarpos"
 filename="output_gpu.png"
 
-./hw3 \
+time ./hw3 \
     $x1 $y1 $z1 \
     $x2 $y2 $z2 \
     $width $height \
     $filename
+
+# ncu ./hw3 \
+#     $x1 $y1 $z1 \
+#     $x2 $y2 $z2 \
+#     $width $height \
+#     $filename
